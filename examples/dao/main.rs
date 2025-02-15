4af3e1bcbe3aed862ce9e422f373650f3c817930
Dr Maxim Orlovsky <orlovsky@ubideco.org>
2025-02-15 22:33:28 +0100

    chore: release v0.12.0-beta.5

--- examples/dao/main.rs
+++ examples/dao/main.rs
@@ -21,6 +21,8 @@
 // or implied. See the License for the specific language governing permissions and limitations under
 // the License.
 
+extern crate alloc;
+
 #[macro_use]
 extern crate amplify;
 #[macro_use]
